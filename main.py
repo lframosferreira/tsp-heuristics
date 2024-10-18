bb2429c8f31655eab2c803cbe36e05b761197b6e
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-10-18 18:07:40 -0300

    input parsing is done

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from util import parse_input_file
 def main() -> None:
     print("Hello TSP")
     graph: nx.Graph = parse_input_file("instances/EUC_2D/pr76.tsp")
-    print(graph)
+    print(graph.get_edge_data(1, 2))
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from math import sqrt
 
 
 def parse_input_file(path: str) -> nx.Graph:
@@ -6,11 +7,13 @@ def parse_input_file(path: str) -> nx.Graph:
     with open(path, "r") as file:
         lines = file.readlines()
         number_of_nodes: int = int(lines[3].split(":")[1])
-        coordinates: list[tuple[int, ...]] = []
+        coordinates: list[tuple[int, ...]] = [(-1, -1)]
         for line in lines[6:]:
-            coordinates.append(tuple(map(lambda x: int(x), line.split(" "))))
-        for i, c1 in enumerate(coordinates):
-            for j, c2 in enumreate(coordinates):
-                
-
+            coordinates.append(tuple(map(lambda x: int(x), line.split(" ")[1:])))
+        for i in range(1, number_of_nodes + 1):
+            for j in range(i + 1, number_of_nodes + 1):
+                c1: tuple[int, ...] = coordinates[i]
+                c2: tuple[int, ...] = coordinates[j]
+                dist: float = sqrt((c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2)
+                graph.add_edge(i, j, weight=dist)
     return graph
