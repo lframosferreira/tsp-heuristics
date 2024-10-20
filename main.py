6249a45d5790149931c4385062e1023489eaecf9
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-10-20 16:55:29 -0300

    started report

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import sys
-
-import networkx as nx
-import tsplib95
-
-from algorithms import christofides, twice_around_the_tree
 from util import get_time_for_every_instance_and_dump
 
 
 def main() -> None:
-    # a = tsplib95.load("instances/EUC_2D/kroE100.tsp")
-    # b = a.get_edges()
-    get_time_for_every_instance_and_dump("twice_around_the_tree")
+    get_time_for_every_instance_and_dump()
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -10,23 +10,34 @@ from algorithms import christofides, twice_around_the_tree
 EUC_2D_DIR: str = "instances/EUC_2D"
 
 
-def get_time_for_every_instance_and_dump(
-    algorithm: str, filename: str = "output.csv"
-) -> None:
-    df: pd.DataFrame = pd.DataFrame(columns=["instance", "path_weight", "time"])
+def get_time_for_every_instance_and_dump(filename: str = "output.csv") -> None:
+    df: pd.DataFrame = pd.DataFrame(
+        columns=["algorithm", "instance", "path_weight", "time"]
+    )
     for file in os.listdir(EUC_2D_DIR):
         graph = tsplib95.load(f"{EUC_2D_DIR}/{file}").get_graph()
         start: float = time.perf_counter()
-        weight: int = 0
-        if algorithm == "twice_around_the_tree":
-            path = twice_around_the_tree(graph)
-            weight = networkx.path_weight(graph, path, "weight")
-        elif algorithm == "christofides":
-            path = christofides(graph)
-            weight = networkx.path_weight(graph, path, "weight")
+        path = twice_around_the_tree(graph)
+        weight = networkx.path_weight(graph, path, "weight")
         end: float = time.perf_counter()
         df = pd.concat(
-            [pd.DataFrame([[file, weight, end - start]], columns=df.columns), df],
+            [
+                pd.DataFrame([["tatt", file, weight, end - start]], columns=df.columns),
+                df,
+            ],
             ignore_index=True,
         )
+        # start: float = time.perf_counter()
+        # path = christofides(graph)
+        # weight = networkx.path_weight(graph, path, "weight")
+        # end: float = time.perf_counter()
+        # df = pd.concat(
+        #     [
+        #         pd.DataFrame(
+        #             [["christofides", file, weight, end - start]], columns=df.columns
+        #         ),
+        #         df,
+        #     ],
+        #     ignore_index=True,
+        # )
     df.to_csv(filename)
