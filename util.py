2ea89b5df72bed08a9836695641f9c9272f52682
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-10-21 10:59:13 -0300

    terminando relatorio da heuristica construtiva

--- util.py
+++ util.py
@@ -8,6 +8,7 @@ import tsplib95
 from algorithms import christofides, twice_around_the_tree
 
 EUC_2D_DIR: str = "instances/EUC_2D"
+NUMBER_OF_EXECUTIONS: int = 20
 
 
 def get_time_for_every_instance_and_dump(filename: str = "output.csv") -> None:
@@ -16,13 +17,20 @@ def get_time_for_every_instance_and_dump(filename: str = "output.csv") -> None:
     )
     for file in os.listdir(EUC_2D_DIR):
         graph = tsplib95.load(f"{EUC_2D_DIR}/{file}").get_graph()
-        start: float = time.perf_counter()
-        path = twice_around_the_tree(graph)
-        weight = networkx.path_weight(graph, path, "weight")
-        end: float = time.perf_counter()
+        total_time: float = 0.0
+        weight: int = 0
+        for _ in range(NUMBER_OF_EXECUTIONS):
+            start: float = time.perf_counter()
+            path = twice_around_the_tree(graph)
+            weight = networkx.path_weight(graph, path, "weight")
+            end: float = time.perf_counter()
+            total_time += end - start
         df = pd.concat(
             [
-                pd.DataFrame([["tatt", file, weight, end - start]], columns=df.columns),
+                pd.DataFrame(
+                    [["tatt", file, weight, total_time / NUMBER_OF_EXECUTIONS]],
+                    columns=df.columns,
+                ),
                 df,
             ],
             ignore_index=True,
