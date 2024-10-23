7fec0bbc24b4cd0f019aa985a149e75328752fcc
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-10-23 09:28:13 -0300

    formatted

--- util.py
+++ util.py
@@ -10,20 +10,28 @@ from algorithms import christofides, twice_around_the_tree
 NUMBER_OF_EXECUTIONS: int = 20
 INSTANCES_FOLDER: str = "instances/tsplib95"
 
-def get_graph_from_tsplib95_file(filepath: str, is_euc_2d: bool = True)->networkx.Graph:
+
+def get_graph_from_tsplib95_file(
+    filepath: str, is_euc_2d: bool = True
+) -> networkx.Graph:
     if is_euc_2d:
         return tsplib95.load(filepath).get_graph()
     else:
         problem = tsplib95.load(filepath)
-        problem._wfunc = lambda start, end: tsplib95.distances.pseudo_euclidean(problem.node_coords[start], problem.node_coords[end])
+        problem._wfunc = lambda start, end: tsplib95.distances.pseudo_euclidean(
+            problem.node_coords[start], problem.node_coords[end]
+        )
         return problem.get_graph()
+
 
 def get_time_for_every_instance_and_dump(filename: str = "output.csv") -> None:
     df: pd.DataFrame = pd.DataFrame(
         columns=["algorithm", "instance", "path_weight", "time"]
     )
     for file in os.listdir(INSTANCES_FOLDER):
-        graph: networkx.Graph = get_graph_from_tsplib95_file(f"{INSTANCES_FOLDER}/{file}", not file.startswith("att"))
+        graph: networkx.Graph = get_graph_from_tsplib95_file(
+            f"{INSTANCES_FOLDER}/{file}", not file.startswith("att")
+        )
         total_time: float = 0.0
         weight: int = 0
         for _ in range(NUMBER_OF_EXECUTIONS):
