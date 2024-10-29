4889e5bf33400c927a3c1b94b5579d491b85dede
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-10-28 22:26:56 -0300

    starting simulated annealing

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,8 @@
 import networkx as nx
+import numpy as np
+
+
+# Constructive algorithms
 
 
 def twice_around_the_tree(graph: nx.Graph, dfs_src_node: int = 1) -> list[int]:
@@ -24,3 +28,55 @@ def christofides(graph: nx.Graph, src: int = 1):
             continue
         hamiltonian_cycle.append(u)
     return hamiltonian_cycle + [src]
+
+
+# Metaheuristic
+
+
+def performe_monte_carlo_steps(
+    number_of_monte_carlo_steps: np.int_,
+    number_of_cities: np.int_,
+    temperature: np.float_,
+    graph: np.array,
+    nodes: np.array,
+    current_path: np.array,
+    current_path_cost: np.float_,
+) -> np.float_:
+    for _ in range(number_of_monte_carlo_steps):
+        for _ in range(number_of_cities):
+            proposed_x, proposed_y = np.sort(
+                np.random.choice(np.arange(number_of_cities), size=2, replace=False)
+            )
+            current_path = np.roll(current_path, shift=1)
+            proposed_path: np.array = current_path.copy()
+            proposed_path[proposed_x + 1 : proposed_y] = np.flip(
+                proposed_path[proposed_x + 1 : proposed_y]
+            )
+
+            remove_edge_1_cost: np.float_ = graph[current_path[proposed_x]][
+                current_path[(proposed_x + 1) % number_of_cities]
+            ]
+            remove_edge_2_cost: np.float_ = graph[current_path[proposed_y - 1]][
+                current_path[proposed_y]
+            ]
+            add_edge_1_cost: np.float_ = graph[proposed_path[proposed_x]][
+                proposed_path[(proposed_x + 1) % number_of_cities]
+            ]
+            add_edge_2_cost: np.float_ = graph[proposed_path[proposed_y - 1]][
+                proposed_path[proposed_y]
+            ]
+
+            decrease: np.float_ = remove_edge_1_cost + remove_edge_2_cost
+            increase: np.float_ = add_edge_1_cost + add_edge_2_cost
+            proposed_path_cost: np.float_ = current_path_cost - decrease + increase
+            delta: np.float_ = proposed_path_cost - current_path_cost
+            r: np.float_ = np.random.rand()
+            P: np.float_ = np.exp(-1 * delta / temperature)
+            if delta < 0 or r <= P:
+                current_path = proposed_path.copy()
+                current_path_cost = proposed_path_cost
+    return current_path, current_path_cost
+
+
+def simulated_annealing(graph: np.array) -> None:
+    pass
